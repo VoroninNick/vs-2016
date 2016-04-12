98651592560b744f33e2c56ee409dc0ae3c0c66a
VoroninNick <p.korenev@voroninstudio.eu>
2016-04-12 13:08:58 +0300

    services 0.2

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160329141557) do
+ActiveRecord::Schema.define(version: 20160409205725) do
 
-  create_table "product_translations", force: :cascade do |t|
-    t.integer  "service_id",   null: false
-    t.string   "locale",       null: false
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.integer  "assetable_id"
+    t.string   "assetable_type",    limit: 30
+    t.string   "type",              limit: 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+  end
+
+  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable"
+  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type"
+
+  create_table "page_translations", force: :cascade do |t|
+    t.integer  "page_id",    null: false
+    t.string   "locale",     null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "url"
+  end
+
+  add_index "page_translations", ["locale"], name: "index_page_translations_on_locale"
+  add_index "page_translations", ["page_id"], name: "index_page_translations_on_page_id"
+
+  create_table "pages", force: :cascade do |t|
+    t.string   "type"
+    t.string   "name"
+    t.text     "content"
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "service_translations", force: :cascade do |t|
+    t.integer  "service_id",       null: false
+    t.string   "locale",           null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.string   "name"
     t.string   "url_fragment"
     t.text     "description"
     t.text     "content"
+    t.string   "descriptive_name"
+    t.text     "long_description"
   end
 
-  add_index "product_translations", ["locale"], name: "index_product_translations_on_locale"
-  add_index "product_translations", ["service_id"], name: "index_product_translations_on_service_id"
+  add_index "service_translations", ["locale"], name: "index_service_translations_on_locale"
+  add_index "service_translations", ["service_id"], name: "index_service_translations_on_service_id"
 
   create_table "services", force: :cascade do |t|
     t.boolean  "published"
@@ -48,6 +86,8 @@ ActiveRecord::Schema.define(version: 20160329141557) do
     t.datetime "list_image_updated_at"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.string   "descriptive_name"
+    t.text     "long_description"
   end
 
 end
