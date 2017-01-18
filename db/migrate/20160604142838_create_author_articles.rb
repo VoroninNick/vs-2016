dbfb54701d062c8413a6770daacbc6c0e6507921
pkorenev <p.korenev@voroninstudio.eu>
2017-01-18 12:53:35 +0200

    app

--- db/migrate/20160604142838_create_author_articles.rb
+++ db/migrate/20160604142838_create_author_articles.rb
@@ -1,7 +1,8 @@
 class CreateAuthorArticles < ActiveRecord::Migration
   def change
     create_table :author_articles do |t|
-      t.belongs_to :author, index: true, foreign_key: true
+      #t.belongs_to :author, index: true, foreign_key: true
+      t.integer :author_id
       t.belongs_to :article, index: true, foreign_key: true
     end
   end
